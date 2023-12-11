750f7a34b697fb8171de3f4ef80f51c51d99ac56
Mirtonmqs <m-marques-gomes@hotmail.com>
2023-12-11 16:39:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+const nome = "mirtonm";
+const idade = "24";
+  return (
+  <p>nome: {nome} </p>
+)
+};
+
+export default App
