f5659da4544d8249cf424268ae40530fe8a81046
Mirtonmqs <m-marques-gomes@hotmail.com>
2023-12-13 15:39:56 -0300

    projeto vite atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar";
+
+import { Outlet } from 'react-router-dom';
+
+import './App.css';
 
 function App() {
-const nome = "mirtonm";
-const idade = "24";
   return (
-  <p>nome: {nome} </p>
-)
-};
+ <div className="App">
+    <Navbar />
+   <div className="container">
+      <Outlet />
+    </div>
+</div>
+ );
+}
 
 export default App
